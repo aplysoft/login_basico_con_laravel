d125948dc426c29e60198051f0175d1b387294c7
giovanni-home <gmlo89@hotmail.com>
2014-01-19 14:41:45 -0700

    Se agrego un toque de diseño

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -3,11 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="/css/bootstrap.css">
 </head>
 <body>
-	<div class="welcome">
+	<div class="container">
 		<h1>Bienvenido {{ Auth::user()->name; }}</h1>
 		<a href="/logout">Cerrar sesión.</a>
 	</div>
+	<script src="https://code.jquery.com/jquery.js"></script>
+    <script src="/js/bootstrap.js"></script>
 </body>
 </html>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,20 +3,38 @@
     <head>
         <meta charset="utf-8">
         <title>Login</title>
+        <link rel="stylesheet" href="/css/bootstrap.css">
     </head>
     <body>
-        {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
-        @if(Session::has('mensaje_error'))
-            {{ Session::get('mensaje_error') }}
-        @endif
-        {{ Form::open(array('url' => '/login')) }}
-            {{ Form::label('usuario', 'Nombre de usuario') }}
-            {{ Form::text('username', Input::old('username')); }}
-            {{ Form::label('contraseña', 'Contraseña') }}
-            {{ Form::password('password'); }}
-            {{ Form::label('lblRememberme', 'Recordar contraseña') }}
-            {{ Form::checkbox('rememberme', true) }}
-            {{ Form::submit('Enviar') }}
-        {{ Form::close() }}
+        <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
+                    @if(Session::has('mensaje_error'))
+                        <div class="alert alert-danger">{{ Session::get('mensaje_error') }}</div>
+                    @endif
+                    {{ Form::open(array('url' => '/login')) }}
+                        <legend>Iniciar sesión</legend>
+                        <div class="form-group">
+                            {{ Form::label('usuario', 'Nombre de usuario') }}
+                            {{ Form::text('username', Input::old('username'), array('class' => 'form-control')); }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::label('contraseña', 'Contraseña') }}
+                            {{ Form::password('password', array('class' => 'form-control')); }}
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                Recordar contraseña
+                                {{ Form::checkbox('rememberme', true) }}
+                            </label>
+                        </div>
+                        {{ Form::submit('Enviar', array('class' => 'btn btn-primary')) }}
+                    {{ Form::close() }}
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <script src="/js/bootstrap.js"></script>
     </body>
 </html>
